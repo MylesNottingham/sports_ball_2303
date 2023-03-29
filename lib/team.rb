1907355b12b6a09541b9e699aa73876daf948382
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-29 10:25:56 -0600

    Write add_player method and refactor class

--- lib/team.rb
+++ lib/team.rb
@@ -1,11 +1,17 @@
-class Player
-  attr_reader  :roster,
-               :player_count
+class Team
+  attr_reader :roster
 
   def initialize(name, city)
     @name = name
     @city = city
     @roster = []
-    @player_count = 0
+  end
+
+  def player_count
+    @roster.count
+  end
+
+  def add_player(player)
+    @roster << player
   end
 end
