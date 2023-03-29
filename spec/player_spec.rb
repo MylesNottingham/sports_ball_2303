a73742187f465cb88371da72782140293fe140a6
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-29 09:56:33 -0600

    Refactor tests by method.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,37 +6,42 @@ RSpec.describe Player do
     @player = Player.new("Michael Palledorous" , 1000000, 36)
   end
 
-  it "exists" do
-    expect(@player).to be_a Player
+  describe "#initialize" do
+
+    it "exists" do
+      expect(@player).to be_a Player
+    end
+
+    it "has a first name" do
+      expect(@player.first_name).to eq("Michael")
+    end
+
+    it "has a last name" do
+      expect(@player.last_name).to eq("Palledorous")
+    end
+
+    it "has a monthly cost" do
+      expect(@player.monthly_cost).to eq(1000000)
+    end
+
+    it "has a contract length" do
+      expect(@player.contract_length).to eq(36)
+    end
+
+    it "has a total cost" do
+      expect(@player.total_cost).to eq(36000000)
+    end
   end
 
-  it "has a first name" do
-    expect(@player.first_name).to eq("Michael")
-  end
+  describe "#set_nickname!" do
+    it "has no nickname by default" do
+      expect(@player.nickname).to eq(nil)
+    end
 
-  it "has a last name" do
-    expect(@player.last_name).to eq("Palledorous")
-  end
+    it "can set a nickname" do
+      @player.set_nickname!("Squints")
 
-  it "has a monthly cost" do
-    expect(@player.monthly_cost).to eq(1000000)
-  end
-
-  it "has a contract length" do
-    expect(@player.contract_length).to eq(36)
-  end
-
-  it "has a total cost" do
-    expect(@player.total_cost).to eq(36000000)
-  end
-
-  it "has no nickname by default" do
-    expect(@player.nickname).to eq(nil)
-  end
-
-  it "can set a nickname" do
-    @player.set_nickname!("Squints")
-
-    expect(@player.nickname).to eq("Squints")
+      expect(@player.nickname).to eq("Squints")
+    end
   end
 end
